3debac40e758eb43e37eb9df7f9f9869fce04fd4
Severus11 <parthsgupta@gmail.com>
2020-07-19 14:04:27 +0530

    init

--- search-sample.py
+++ search-sample.py
@@ -6,6 +6,14 @@ filename = 'sample.jpg'
 image = mpimg.imread(filename)
 #print(image.dtype,image.shape, np.min(image), np.max(image))
 # uint8, 160,320,3 0-255 8bit coloring
-plt.imshow(image)
-plt.show()
+#plt.imshow(image)
+#plt.show()
+def color_thresh(img, rgb_thresh=(0,0,0)):
+    color_sel = np.zeros_like(img[:,:,0])
+    above_thresh = (img[:,:,0] > rgb_thresh[0])& (img[:,:,1] > rgb_thresh[1])& (img[:,:,2] > rgb_thresh[2])
+    #if(img[:,:,0]>rgb_thresh[0] & img[:,:,1]>rgb_thresh[1] & img[:,:,2]>rgb_thresh[2]):
+    color_sel[above_thresh]=1
+    return color_sel
 
+plt.imshow(color_thresh(image, rgb_thresh=(160,160,160)))
+plt.show()
