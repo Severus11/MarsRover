c047bb0aad92573d3c1f9d4d796a40c6e2e94821
Severus11 <parthsgupta@gmail.com>
2020-07-20 03:29:35 +0530

    generated world map

--- generate_map.py
+++ generate_map.py
@@ -2,48 +2,62 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import cv2
-from perspectivetf import perspectiveTF, color_thresh,rover_centric_coord
+
+def perspect_transform(img, src, dst):
+    M = cv2.getPerspectiveTransform(src, dst)
+    warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))
+    return warped
+
+def color_thresh(img, rgb_thresh=(170, 170, 170)):
+    color_select = np.zeros_like(img[:,:,0])
+    above_thresh = (img[:,:,0] > rgb_thresh[0])& (img[:,:,1] > rgb_thresh[1]) & (img[:,:,2] > rgb_thresh[2])
+    color_select[above_thresh] = 1
+    return color_select
+    
+def rover_coords(binary_img):
+    ypos, xpos = binary_img.nonzero() 
+    x_pixel = np.absolute(ypos - binary_img.shape[0]).astype(np.float)
+    y_pixel = -(xpos - binary_img.shape[0]).astype(np.float)
+    return x_pixel, y_pixel
+
 image = mpimg.imread('sample.jpg')
 rover_yaw = np.random.random(1)*360
 rover_xpos = np.random.random(1)*160 + 20
 rover_ypos = np.random.random(1)*160 + 20
 
-def rot(x_pixel, y_pixel, yaw):
-    yaw_rad= yaw*np.pi/180
-    xrot = x_pixel* np.cos(yaw_rad) - y_pixel* np.sin(yaw_rad)
-    yrot = x_pixel* np.cos(yaw_rad) + y_pixel* np.sin(yaw_rad)    
-    return xrot, yrot
-def trans( x_pos, y_pos, xrot, yrot, scale):
-    xw= np.int_(x_pos + (xrot/scale))
-    yw= np.int_(y_pos + (yrot/scale))
-    #truncating transalted values between 0 and 200 (world size taken 200)
-    x_ts= np.clip(xw,0,199)
-    y_ts= np.clip(yw,0,199)
-    return x_ts, y_ts
-def transform(xpix, ypix, xpos, ypos, yaw, world_size, scale):
-    xpix_rot, ypix_rot = rot(xpix, ypix, yaw)
-    xpix_tran, ypix_tran = trans(xpos, ypos,xpix_rot, ypix_rot,scale)
-    #x_pix_world = np.clip(np.int_(xpix_tran), 0, world_size - 1)
-    #y_pix_world = np.clip(np.int_(ypix_tran), 0, world_size - 1)
-    return xpix_tran, ypix_tran
+def rotate_pix(xpix, ypix, yaw):
+    yaw_rad = yaw * np.pi / 180
+    xpix_rotated = (xpix * np.cos(yaw_rad)) - (ypix * np.sin(yaw_rad))                            
+    ypix_rotated = (xpix * np.sin(yaw_rad)) + (ypix * np.cos(yaw_rad))
+    return xpix_rotated, ypix_rotated
 
-dst_size= 5
-bottom_offset =6
-u= image.shape[1]/2
-v= image.shape[0]/2
+def translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale): 
+    xpix_translated = (xpix_rot / scale) + xpos
+    ypix_translated = (ypix_rot / scale) + ypos
+  
+    return xpix_translated, ypix_translated
 
-source= np.float32([[14, 140], [301 ,140],[200, 96], [118, 96]])
-dest =np.float32([[image.shape[1]/2 - dst_size, image.shape[0] - bottom_offset],[image.shape[1]/2 + dst_size, image.shape[0] - bottom_offset],[image.shape[1]/2 + dst_size, image.shape[0] - 2*dst_size - bottom_offset], [image.shape[1]/2 - dst_size, image.shape[0] - 2*dst_size - bottom_offset],])
+def pix_to_world(xpix, ypix, xpos, ypos, yaw, world_size, scale):
+    xpix_rot, ypix_rot = rotate_pix(xpix, ypix, yaw)
+    xpix_tran, ypix_tran = translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale)
+    x_pix_world = np.clip(np.int_(xpix_tran), 0, world_size - 1)
+    y_pix_world = np.clip(np.int_(ypix_tran), 0, world_size - 1)
+    return x_pix_world, y_pix_world
 
-warp= perspectiveTF(image, source, dest)
-color_sel= color_thresh(warp, rgb_thresh=(160,160,160))
-xpix, ypix = rover_centric_coord(color_sel)
+dst_size = 5 
+bottom_offset = 6
+source = np.float32([[14, 140], [301 ,140],[200, 96], [118, 96]])
+destination = np.float32([[image.shape[1]/2 - dst_size, image.shape[0] - bottom_offset],[image.shape[1]/2 + dst_size, image.shape[0] - bottom_offset],[image.shape[1]/2 + dst_size, image.shape[0] - 2*dst_size - bottom_offset], [image.shape[1]/2 - dst_size, image.shape[0] - 2*dst_size - bottom_offset],])
+warped = perspect_transform(image, source, destination)
+colorsel = color_thresh(warped, rgb_thresh=(160, 160, 160))
+xpix, ypix = rover_coords(colorsel)
 
-worldmap = np.zeros((200,200))
-scale =10
+worldmap = np.zeros((200, 200))
+scale = 10
+x_world, y_world = pix_to_world(xpix, ypix, rover_xpos, rover_ypos, rover_yaw, worldmap.shape[0], scale)
 
-x_world, y_world= transform(xpix, ypix,rover_xpos, rover_ypos, rover_yaw, worldmap[0], scale)
-worldmap[y_world, x_world]+=1
+worldmap[y_world, x_world] += 1
+print('Xpos =', rover_xpos, 'Ypos =', rover_ypos, 'Yaw =', rover_yaw)
 
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 7))
 f.tight_layout()
@@ -58,3 +72,6 @@ ax2.set_title('World Space', fontsize=40)
 ax2.set_ylim(0, 200)
 ax2.tick_params(labelsize=20)
 ax2.set_xlim(0, 200)
+
+plt.subplots_adjust(left=0.1, right=1, top=0.9, bottom=0.1)
+plt.show() 
