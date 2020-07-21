dc9239906034c5d05ec11b149da648c204294126
Severus11 <parthsgupta@gmail.com>
2020-07-22 00:48:46 +0530

    completed perception

--- perception/Test_bench.py
+++ perception/Test_bench.py
@@ -21,12 +21,12 @@ ground_map_3d = np.dstack((ground_map*0, ground_map*255, ground_map*0))
 
 class Databucket():
     def __init__(self):
-        self.images= csv_img_list
-        self.xpos = df["X_Positon"].values
+        self.images = csv_img_list  
+        self.xpos = df["X_Position"].values
         self.ypos = df["Y_Position"].values
-        self.yaw  = df["Yaw"].values
+        self.yaw = df["Yaw"].values
         self.count = 0
-        self.worldmap = np.zeros((200,200,3))
+        self.worldmap = np.zeros((200, 200, 3)).astype(np.float)
         self.ground_map = ground_map_3d
 
 data = Databucket()
@@ -36,10 +36,13 @@ def process_image(img):
     output_image[0:img.shape[0], 0:img.shape[1]] = img
     source = np.float32([[14, 140], [301 ,140],[200, 96], [118, 96]])
     destination = np.float32([[img.shape[1]/2 - dst_size, img.shape[0] - bottom_offset],[img.shape[1]/2 + dst_size, img.shape[0] - bottom_offset],[img.shape[1]/2 + dst_size, img.shape[0] - 2*dst_size - bottom_offset], [img.shape[1]/2 - dst_size, img.shape[0] - 2*dst_size - bottom_offset],])
+    
     warped = perspect_transform(img, source, destination)
     output_image[0:img.shape[0], img.shape[1]:] = warped
-    map_add = cv2.addWeighted(data.worldmap, 1, data.ground_truth, 0.5, 0)
+    
+    map_add = cv2.addWeighted(data.worldmap, 1, data.ground_map, 0.5, 0,dtype = cv2.CV_32F)
     output_image[img.shape[0]:, 0:data.worldmap.shape[1]] = np.flipud(map_add)
+    
     cv2.putText(output_image,"Populate this image with your analyses to make a video!", (20, 20), 
                 cv2.FONT_HERSHEY_COMPLEX, 0.4, (255, 255, 255), 1)
     if data.count < len(data.images) - 1:
